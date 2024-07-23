19a086fdc1e84d32e01d149b897adbba4014346f
mstflotfy <mstf.lotfy@gmail.com>
2024-07-23 16:08:52 +0300

    setup default font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss"
+const { fontFamily } = require("tailwindcss/defaultTheme")
 
 const config = {
   darkMode: ["class"],
@@ -19,7 +20,7 @@ const config = {
     },
     extend: {
       fontFamily: {
-        
+        sans: ["var(--font-sans)", ...fontFamily.sans],
       },
       colors: {
         border: "hsl(var(--border))",
