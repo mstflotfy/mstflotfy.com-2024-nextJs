9cd85ed521fd2ec767664d64c3142d0921ffb776
mstflotfy <mstf.lotfy@gmail.com>
2024-07-25 18:16:49 +0300

    add the footer and set container style

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/footer";
 import Hero from "@/components/hero";
 import Nav from "@/components/nav";
 
@@ -5,45 +6,10 @@ export default function Home() {
   return (
     <main>
       <Nav />
+      
       <Hero />
-      <p>
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        To apply the equivalent styles in Tailwind CSS, you can use the following classes:
-        sticky: Applies position: sticky.
-      </p>
+      
+      <Footer />
     </main>
   );
 }
---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer
+      className="bg-m3-sys-light-surface-variant h-[41rem] mt-44 py-[6.4rem] text-center text-m3-sys-light-on-surface-variant"
+    >
+      <small>&copy; Mostafa Lotfy (mstflotfy) 2024. All rights reserved.</small>
+    </footer>
+  )
+}
+
+export default Footer
