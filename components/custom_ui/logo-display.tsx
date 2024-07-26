a937711b1e6c99dcd52277bf12991cda0a4c5a0a
mstflotfy <mstf.lotfy@gmail.com>
2024-07-26 16:10:23 +0300

    adjust gap

--- components/custom_ui/logo-display.tsx
+++ components/custom_ui/logo-display.tsx
@@ -1,7 +1,7 @@
 const LogoDisplay = ({children, description=''}) => {
   return (
     <div
-      className="flex flex-col items-center"
+      className="flex flex-col items-center space-y-4"
     >
       {children}
       <p
