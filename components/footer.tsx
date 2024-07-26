ececa3de8d5b5df98dc3725af7fa64964faa46ec
mstflotfy <mstf.lotfy@gmail.com>
2024-07-26 16:54:11 +0300

    adjust small text tracking & add dynamic year
    Closes #1

--- components/footer.tsx
+++ components/footer.tsx
@@ -10,28 +10,30 @@ const Footer = () => {
   return (
     <footer
       className="bg-m3-sys-light-surface-variant h-[41rem] mt-44 pt-[6.4rem] p-[1.8rem] text-center text-m3-sys-light-on-surface-variant flex flex-col justify-between"
+      aria-label="Site footer"
     >
     
     <FooterNav />
     
-    <div>
-    <section className="flex space-x-8 text-title-large p-10 justify-center">
-      <CustomLink href={externalLinks.indieDev} target="_blank" rel="noopener" aria-label="youtube">
-        <FaYoutube />
-      </CustomLink>
-        <CustomLink href={externalLinks.github} target="_blank" rel="noopener" aria-label="GitHub">
-          <FaGithub />
+    <section>
+      <section aria-label="Social Medi Links" className="flex space-x-8 text-title-large p-10 justify-center">
+        <CustomLink href={externalLinks.indieDev} target="_blank" rel="noopener" aria-label="youtube">
+          <FaYoutube />
         </CustomLink>
-        <CustomLink href={externalLinks.contact} target="_blank" rel="noopener" aria-label="Email">
-          <MdEmail />
-        </CustomLink>
-        <CustomLink href={externalLinks.figma} target="_blank" rel="noopener" aria-label="Figma">
-          <IoLogoFigma />
-        </CustomLink>
+          <CustomLink href={externalLinks.github} target="_blank" rel="noopener" aria-label="GitHub">
+            <FaGithub />
+          </CustomLink>
+          <CustomLink href={externalLinks.contact} target="_blank" rel="noopener" aria-label="Email">
+            <MdEmail />
+          </CustomLink>
+          <CustomLink href={externalLinks.figma} target="_blank" rel="noopener" aria-label="Figma">
+            <IoLogoFigma />
+          </CustomLink>
+        </section>
+        
+        <small aria-label="copy rights">&copy; Mostafa Lotfy (mstflotfy) {new Date().getFullYear()}. All rights reserved.</small>
       </section>
       
-      <small>&copy; Mostafa Lotfy (mstflotfy) 2024. All rights reserved.</small>
-      </div>
     </footer>
   )
 }
