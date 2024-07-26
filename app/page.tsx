ef5c539896a5034c3c7a6781cbd94043d0fd131a
mstflotfy <mstf.lotfy@gmail.com>
2024-07-26 22:53:41 +0300

    add app-showcase-device comp

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
 import Footer from "@/components/footer";
 import Hero from "@/components/hero";
 import Nav from "@/components/nav";
+import DeviceFrame from "@/components/oneExercise-showcase";
+import externalLinks from "@/lib/external-links";
 
 export default function Home() {
   return (
-    <main>
+    <main
+      className="space-y-12"
+    >
       <Nav />
-      
       <Hero />
-      
+      <DeviceFrame 
+        src={externalLinks.OneExercise}
+        title="Try 1Xercise (OneExercise)"
+        name="OneExercise-iframe"
+        aria-label="This iframe showcases my workout tracker app (1Xercsie)"
+      />
       <Footer />
     </main>
   );
