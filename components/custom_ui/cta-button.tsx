cea6cc0f02e5eb0da1335612ae8d880da70c4210
mstflotfy <mstf.lotfy@gmail.com>
2024-07-27 18:14:15 +0300

    add cta action btn comp

---
+++ components/custom_ui/cta-button.tsx
@@ -0,0 +1,10 @@
+const CtaButton = ({children}) => {
+ return (
+   <div
+     className="bg-m3-sys-light-primary-container text-m3-sys-light-on-primary-container p-[1.6rem] rounded-[1.6rem]"
+   >
+    {children}
+   </div>
+ ) 
+}
+export default CtaButton
