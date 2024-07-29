1cc916c7b025796923ae35011b61382bcfd1cbd7
mstflotfy <mstf.lotfy@gmail.com>
2024-07-29 21:12:59 +0300

    edit social-links colors and hover

--- components/custom_ui/custom-external-link-icon.jsx
+++ components/custom_ui/custom-external-link-icon.jsx
@@ -1,6 +1,6 @@
 const CustomLink = ({children, href="/", target="_blank", rel="noopener", ariaLabel=""}) => {
   return (
-    <a href={href} target={target} rel={rel} aria-label={ariaLabel} className="hover:text-m3-white active:text-m3-white focus:text-m3-white transition-colors duration-200">
+    <a href={href} target={target} rel={rel} aria-label={ariaLabel} className="text-m3-sys-light-on-surface hover:text-m3-sys-light-secondary active:text-m3-sys-light-secondary focus:text-m3-sys-light-secondary transition-colors duration-200">
       {children}
     </a>
   )
