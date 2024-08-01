0c72b2de5ba29c3d8f9afa2f78fefe8f254eb61c
mstflotfy <mstf.lotfy@gmail.com>
2024-08-01 20:44:57 +0300

    add more space above 1xercise logo

--- app/oneexercise/page.tsx
+++ app/oneexercise/page.tsx
@@ -5,7 +5,7 @@ export default function OneExercise() {
       <main>
         <header 
           aria-label="Page header"
-          className="flex items-center p-16"
+          className="flex items-center p-16 mt-[1.6rem]"
         >
           <OneExerciseLogo  full />
         </header>
