e4213700bb49f9ccded49a2e3f0eeb76b173e502
mstflotfy <mstf.lotfy@gmail.com>
2024-08-02 01:48:58 +0300

    make border optional

--- components/app-showcase.tsx
+++ components/app-showcase.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import DeviceFrame from "./custom_ui/device"
 import { ReactNode } from "react"
 
@@ -8,6 +9,7 @@ interface AppShowCaseProps {
   ariaLabel?: string;
   left?: ReactNode;
   right?: ReactNode;
+  border: boolean;
 }
 
 const AppShowCase: React.FC<AppShowCaseProps> = ({
@@ -16,12 +18,18 @@ const AppShowCase: React.FC<AppShowCaseProps> = ({
   name='', 
   ariaLabel='an iframe that showcases an app I created', 
   left = null, 
-  right = null
+  right = null,
+  border = true
 }) => {
   return (
     <div
-      className="relative flex flex-col items-center justify-center lg:flex-row gap-[3.6rem] border-2 xl:border-none py-[3.4rem] mx-12 text-center xl:text-left"
-    >
+      className={
+        cn(
+          "relative flex flex-col items-center justify-center lg:flex-row gap-[3.6rem] py-[3.4rem] mx-12 text-center xl:text-left",
+          border && 'border-2 xl:border-none',
+          )
+      }
+        >
       {left}
       
       <DeviceFrame 
@@ -33,10 +41,13 @@ const AppShowCase: React.FC<AppShowCaseProps> = ({
       
       {right}
       
-      <div
-        className="border-2 absolute top-0 xl:top-[17.8rem] bottom-0 xl:bottom-[17.8rem] w-[400px] sm:w-auto sm:right-[5.7rem] sm:left-[5.7rem] hidden xl:block"
-      >
-      </div>
+      {
+        border &&
+        <div
+          className="border-2 absolute top-0 xl:top-[17.8rem] bottom-0 xl:bottom-[17.8rem] w-[400px] sm:w-auto sm:right-[5.7rem] sm:left-[5.7rem] hidden xl:block"
+        >
+        </div>
+      }
       
     </div>
   )
