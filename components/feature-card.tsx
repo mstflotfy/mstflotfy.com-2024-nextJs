7cc87f5fb3ddde33638c3e6dfffe260e3cb99144
mstflotfy <mstf.lotfy@gmail.com>
2024-08-06 07:24:28 +0300

    seperate feature-card comp

---
+++ components/feature-card.tsx
@@ -0,0 +1,45 @@
+import { cn } from "@/lib/utils"
+import { roboto_mono } from "@/lib/fonts"
+import { Button } from "./ui/button"
+
+interface FeatureButtonProps {
+  feature: string
+  description: string
+  onclick: React.MouseEventHandler
+  active: boolean
+}
+const FeatureCard: React.FC<FeatureButtonProps> = ({
+  feature, 
+  description, 
+  onclick, 
+  active = false
+}) => {
+  return (
+    <>
+    <Button
+      variant="outline"
+      onClick={onclick}
+      className={
+        cn(
+          "flex flex-col gap-[1.6rem] p-[0.9rem] py-[2.1rem] h-auto rounded-[1.2rem] w-[30.6rem] hover:bg-m3-sys-light-secondary-container transition-colors",
+          active && "bg-m3-sys-light-primary-container hover:bg-m3-sys-light-primary-container"
+          )
+      }
+    >
+      <h3
+        className={
+          cn(
+            roboto_mono.className,
+            "text-center text-headline-small leading-headline-small"
+          )
+        }
+        >{feature.toUpperCase()}
+      </h3>
+      <p
+        className="text-wrap text-body-medium leading-body-medium"
+      >{description}</p>
+    </Button>
+    </>
+  )
+}
+export default FeatureCard
--- components/feature-showcase.tsx
+++ components/feature-showcase.tsx
@@ -2,49 +2,10 @@
 
 import externalLinks from "@/lib/external-links"
 import Image from "next/image"
-import { cn } from "@/lib/utils"
-import { roboto_mono } from "@/lib/fonts"
-import { Button } from "./ui/button"
 import DeviceFrame from "./custom_ui/device"
 import { useState } from "react"
 import { oneexerciseFeatures } from "@/lib/data"
-
-interface FeatureButtonProps {
-  feature: string
-  description: string
-  onclick: React.MouseEventHandler
-  active: boolean
-}
-
-const FeatureCard: React.FC<FeatureButtonProps> = ({feature, description, onclick, active = false}) => {
-  return (
-    <>
-    <Button
-      variant="outline"
-      onClick={onclick}
-      className={
-        cn(
-          "flex flex-col gap-[1.6rem] p-[0.9rem] py-[2.1rem] h-auto rounded-[1.2rem] w-[30.6rem] hover:bg-m3-sys-light-secondary-container transition-colors",
-          active && "bg-m3-sys-light-primary-container hover:bg-m3-sys-light-primary-container"
-          )
-      }
-    >
-      <h3
-        className={
-          cn(
-            roboto_mono.className,
-            "text-center text-headline-small leading-headline-small"
-          )
-        }
-        >{feature.toUpperCase()}
-      </h3>
-      <p
-        className="text-wrap text-body-medium leading-body-medium"
-      >{description}</p>
-    </Button>
-    </>
-  )
-}
+import FeatureCard from "./feature-card"
 
 export default function FeatureShowcase()  {
   const [ activeFeature, setActiveFeature ] = useState(0)
