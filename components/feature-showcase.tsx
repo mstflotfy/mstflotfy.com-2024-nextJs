94c225b57adf3a5d066fad349e0888efa018c491
mstflotfy <mstf.lotfy@gmail.com>
2024-08-06 07:18:02 +0300

    mv comp data into lib

--- components/feature-showcase.tsx
+++ components/feature-showcase.tsx
@@ -3,13 +3,11 @@
 import externalLinks from "@/lib/external-links"
 import Image from "next/image"
 import { cn } from "@/lib/utils"
-
 import { roboto_mono } from "@/lib/fonts"
-
 import { Button } from "./ui/button"
 import DeviceFrame from "./custom_ui/device"
 import { useState } from "react"
-
+import { oneexerciseFeatures } from "@/lib/data"
 
 interface FeatureButtonProps {
   feature: string
@@ -50,54 +48,6 @@ const FeatureCard: React.FC<FeatureButtonProps> = ({feature, description, onclic
 
 export default function FeatureShowcase()  {
   const [ activeFeature, setActiveFeature ] = useState(0)
-  
-  const oneexerciseFeatures = [
-    {
-      name: 'FREE',
-      description: 'OneExercise core features are free and will always be.',
-      src: '/images/OneExercise-feat-free.jpg'
-    },
-    {
-      name: 'PRAVIVACY FOCUSED',
-      description: 'OneExrcise does not track your activity, does not collect your data, and does not sell your data.',
-      src: '/images/OneExercise-feat-free.jpg'
-    },
-    {
-      name: 'WEB APP',
-      description: 'You can download it as a PWA. Or just open your browser and start tracking your workouts.',
-      src: '/images/OneExercise-feat-free.jpg'
-    },
-    {
-      name: 'Works Offline',
-      description: 'Offline-first. Auto-saves to your browser.',
-      src: '/images/OneExercise-feat-free.jpg'
-    },
-    {
-      name: 'Export & Import',
-      description: 'Back up your progress, or switch browsers, by exporting and importing your workouts and exercises.',
-      src: '/images/OneExercise-feat-free.jpg'
-    },
-    {
-      name: 'Stats',
-      description: 'OneExercise compares each new set to the very previous workout (for the same exercise) and gives you instant color coded feedback. And gives you other stats to track your progress.',
-      src: '/images/OneExercise-feat-free.jpg'
-    },
-    {
-     name: 'Custom Exercises',
-     description: 'Start with a short list of preset core exercises. You can also create your own list of Exercises and decide what you want to track for each exercise',
-     src: '/images/OneExercise-feat-free.jpg'
-    },
-    {
-      name: 'Dark Mode',
-      description: 'Choose the mode that best suits your environment. Or just let it follow your system’ color mode.',
-      src: '/images/OneExercise-feat-free.jpg'
-    },
-    {
-      name: 'And more...',
-      description: 'OneExercise is an active project so expect new features to pop up',
-      src: '/images/OneExercise-feat-free.jpg'
-    }
-  ]
   
   return (
     <div
---
+++ lib/data.ts
@@ -0,0 +1,53 @@
+
+export interface Feature {
+  name: string;
+  description: string;
+  src: string;
+}
+export const oneexerciseFeatures: Feature[] = [
+  {
+    name: 'FREE',
+    description: 'OneExercise core features are free and will always be.',
+    src: '/images/OneExercise-feat-free.jpg'
+  },
+  {
+    name: 'PRAVIVACY FOCUSED',
+    description: 'OneExrcise does not track your activity, does not collect your data, and does not sell your data.',
+    src: '/images/OneExercise-feat-free.jpg'
+  },
+  {
+    name: 'WEB APP',
+    description: 'You can download it as a PWA. Or just open your browser and start tracking your workouts.',
+    src: '/images/OneExercise-feat-free.jpg'
+  },
+  {
+    name: 'Works Offline',
+    description: 'Offline-first. Auto-saves to your browser.',
+    src: '/images/OneExercise-feat-free.jpg'
+  },
+  {
+    name: 'Export & Import',
+    description: 'Back up your progress, or switch browsers, by exporting and importing your workouts and exercises.',
+    src: '/images/OneExercise-feat-free.jpg'
+  },
+  {
+    name: 'Stats',
+    description: 'OneExercise compares each new set to the very previous workout (for the same exercise) and gives you instant color coded feedback. And gives you other stats to track your progress.',
+    src: '/images/OneExercise-feat-free.jpg'
+  },
+  {
+   name: 'Custom Exercises',
+   description: 'Start with a short list of preset core exercises. You can also create your own list of Exercises and decide what you want to track for each exercise',
+   src: '/images/OneExercise-feat-free.jpg'
+  },
+  {
+    name: 'Dark Mode',
+    description: 'Choose the mode that best suits your environment. Or just let it follow your system’ color mode.',
+    src: '/images/OneExercise-feat-free.jpg'
+  },
+  {
+    name: 'And more...',
+    description: 'OneExercise is an active project so expect new features to pop up',
+    src: '/images/OneExercise-feat-free.jpg'
+  }
+]
