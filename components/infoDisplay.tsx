b5c0b8ee412482459e47cc6d32c7e0b95037779a
mstflotfy <mstf.lotfy@gmail.com>
2024-08-08 01:06:58 +0300

    org comp

--- components/infoDisplay.tsx
+++ components/infoDisplay.tsx
@@ -26,112 +26,113 @@ export default function InfoDisplay()  {
       className="flex flex-col lg:flex-row place-items-center lg:gap-[5.2rem] min-h-[77.8rem] px-[3.2rem] justify-center" 
     >
     
-      <div
-        className="hidden lg:flex gap-[0.6rem] max-w-[61.8rem] border-[0.3rem] rounded-[1.2rem] min-w-[74.4rem] min-h-[67.2rem]"
-      >
-      
-        <ul
-          aria-label="table of content"
-          className="flex flex-col border-r-[0.2rem] py-[4.9rem] px-[2.6rem] gap-[1.5rem] text-label-medium"
-          
+      {/* Table for larger devices */}
+        <div
+          className="hidden lg:flex gap-[0.6rem] max-w-[61.8rem] border-[0.3rem] rounded-[1.2rem] min-w-[74.4rem] min-h-[67.2rem]"
         >
-          {
-            oneExerciseContent.map((content, index) => (
-              <li
-                className={`
-                    text-nowrap cursor-pointer hover:text-m3-sys-light-secondary transition-all duration-100 ease-in-out"
-                    ${index === activeContent && 'text-m3-sys-light-primary hover:text-m3-sys-light-primary'}
-                    ${index === activeContent && 'text-label-large'}
-                  `}
-                key={index}
-                onClick={() => setActiveContent(index)}
-              >
-                {content.name}
-              </li>
-            ))
-          }
-        </ul>
-      
-      <div
-        className="py-[4.9rem] px-[2.6rem] text-body-large leading-body-large tracking-body-large space-y-8"
-      >
-        {
-          oneExerciseContent[activeContent].description.split('\n').map((paragraph, index) => (
-            <p
-              key={index}
-            >
-              {paragraph}
-            </p>
-          ))
-        }
-      </div>
-      
-      </div>
-      
-      <Carousel
-        className="lg:hidden w-full m-auto"
-        opts={{
-          align: "center",
-          loop: true
-        }}
-      >
-        <CarouselContent
-          className="-mx-[2.6rem] "
-        >
-          {
-            oneExerciseContent.map((content, index) => (
-              <CarouselItem
-                className="flex flex-col items-center gap-[0.6rem] basis-[90%]"
-                key={index}
-              >
-              
-                <FeatureCard
-                  feature={ content.name}
-                  description={content.description}
-                  active={true}
-                  onclick={() => {}}
-                  className="w-[90%]"
-                />
-                <small
-                  className="text-center"
-                >{index + 1} of {contentLength}</small>
-                
-                <DeviceFrame 
-                  src={externalLinks.OneExercise}
-                  small
-                  name="OneExercise"
-                  customWidth="w-[80%]"
+        
+          <ul
+            aria-label="table of content"
+            className="flex flex-col border-r-[0.2rem] py-[4.9rem] px-[2.6rem] gap-[1.5rem] text-label-medium"
+            
+          >
+            {
+              oneExerciseContent.map((content, index) => (
+                <li
+                  className={`
+                      text-nowrap cursor-pointer hover:text-m3-sys-light-secondary transition-all duration-100 ease-in-out"
+                      ${index === activeContent && 'text-m3-sys-light-primary hover:text-m3-sys-light-primary'}
+                      ${index === activeContent && 'text-label-large'}
+                    `}
+                  key={index}
+                  onClick={() => setActiveContent(index)}
                 >
-                  <Image
-                    src={oneExerciseContent[activeContent].src}
-                    alt={`OneExercise app feature: ${oneExerciseContent[activeContent].name}`}
-                    width={313.7}
-                    height={594.44}
-                  />
-                </DeviceFrame>
-                
-              </CarouselItem>
+                  {content.name}
+                </li>
+              ))
+            }
+          </ul>
+        
+        <div
+          className="py-[4.9rem] px-[2.6rem] text-body-large leading-body-large tracking-body-large space-y-8"
+        >
+          {
+            oneExerciseContent[activeContent].description.split('\n').map((paragraph, index) => (
+              <p
+                key={index}
+              >
+                {paragraph}
+              </p>
             ))
           }
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
+        </div>
+        
+        </div>
+        <DeviceFrame 
+          src={externalLinks.OneExercise}
+          small
+          name="OneExercise"
+          className="hidden lg:flex"
+        >
+          <Image
+            src={oneExerciseContent[activeContent].src}
+            alt={`OneExercise app feature: ${oneExerciseContent[activeContent].name}`}
+            width={313.7}
+            height={594.44}
+          />
+        </DeviceFrame>
       
+      {/* for smaller devices switch to a carousel */}
+        <Carousel
+          className="lg:hidden w-full m-auto"
+          opts={{
+            align: "center",
+            loop: true
+          }}
+        >
+          <CarouselContent
+            className="-mx-[2.6rem] "
+          >
+            {
+              oneExerciseContent.map((content, index) => (
+                <CarouselItem
+                  className="flex flex-col items-center gap-[0.6rem] basis-[90%]"
+                  key={index}
+                >
+                
+                  <FeatureCard
+                    feature={ content.name}
+                    description={content.description}
+                    active={true}
+                    onclick={() => {}}
+                    className="w-[90%]"
+                  />
+                  <small
+                    className="text-center"
+                  >{index + 1} of {contentLength}</small>
+                  
+                  <DeviceFrame 
+                    src={externalLinks.OneExercise}
+                    small
+                    name="OneExercise"
+                    customWidth="w-[80%]"
+                  >
+                    <Image
+                      src={oneExerciseContent[activeContent].src}
+                      alt={`OneExercise app feature: ${oneExerciseContent[activeContent].name}`}
+                      width={313.7}
+                      height={594.44}
+                    />
+                  </DeviceFrame>
+                  
+                </CarouselItem>
+              ))
+            }
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
       
-      <DeviceFrame 
-        src={externalLinks.OneExercise}
-        small
-        name="OneExercise"
-        className="hidden lg:flex"
-      >
-        <Image
-          src={oneExerciseContent[activeContent].src}
-          alt={`OneExercise app feature: ${oneExerciseContent[activeContent].name}`}
-          width={313.7}
-          height={594.44}
-        />
-      </DeviceFrame>
     </div>
   )
 }
