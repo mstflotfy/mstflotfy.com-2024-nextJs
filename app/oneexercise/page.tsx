87b1b8977851b4956895fe437756508b41942258
mstflotfy <mstf.lotfy@gmail.com>
2024-08-09 03:13:41 +0300

    fix page spacing

--- app/oneexercise/page.tsx
+++ app/oneexercise/page.tsx
@@ -11,11 +11,12 @@ export default function OneExercise() {
       <main>
         
         <div
-          className="bg-m3-custom-light-green -mt-[7rem] pt-[10rem] pb-[6.8rem]" 
+          className="bg-m3-custom-light-green -mt-[7rem] pt-[10.6rem] md:pt-[10rem] pb-[4.6rem] md:pb-[6.8rem] " 
+          // className="bg-m3-custom-light-green pt-[6.3rem] space-y-[4.6rem] pb-[4.6rem] md:pt-[10rem] md:pb-[21.8rem]" 
         >
         <header 
           aria-label="Page header"
-          className="flex items-center p-16 mt-[1.6rem]"
+          className="flex items-center md:p-16 mt-0 md:mt-[1.6rem]"
         >
           <OneExerciseLogo  full />
         </header>
@@ -30,16 +31,17 @@ export default function OneExercise() {
             labelTop="Try it here!"
             left={
               <div
-                className="flex flex-col place-items-end gap-[0.6rem]"
+                className="flex flex-col lg:place-items-end gap-[0.6rem]"
               >
                 <p
                   className={
                     cn(
                       gloria.className,
-                      "text-m3-sys-light-primary"
+                      "text-m3-sys-light-primary",
+                      "text-nowrap"
                     )
                   }
-                >The single-exercise workout tracker</p>
+                >The single-exercise workout-tracker</p>
                 <p
                   className={
                     cn(
@@ -56,18 +58,18 @@ export default function OneExercise() {
           </div>
         
         <section
-         className="bg-m3-custom-light-yellow pt-[10rem] pb-[21.8rem]" 
+         className="bg-m3-custom-light-yellow pt-[6.3rem] space-y-[4.6rem] pb-[4.6rem] md:pt-[10rem] md:pb-[21.8rem]" 
         >
           <h2
           className={
             cn(
-              "text-headline-large md:text-mstf-h2 text-center mb-[7.8rem]",
+              "text-headline-large md:text-mstf-h2 text-center max-w-[370px] md:max-w-[650px] leading-mstf-headline-large tracking-mstf-headline-large mx-auto md:mb-[7.8rem]",
               gloria.className
               )
           }
           >Features</h2>
           
-          {/* <FeatureShowcase /> */}
+          <FeatureShowcase />
         </section>
 
         <section
