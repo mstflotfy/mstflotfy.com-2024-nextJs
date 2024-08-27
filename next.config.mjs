e047fc49f18c2d59a1e17663ec10c95a2229e494
mstflotfy <mstf.lotfy@gmail.com>
2024-08-27 17:22:56 +0300

    add permenant 301 redirects for old site posts
    - redirect old posts from old urls (mstflotfy.com/dev/postname or mstflotfy.com/deisgn/post to mstflotfy.com/theindiedev/post) for a simpler app structure

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,22 @@ const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
   transpilePackages: ['next-mdx-remote'],
+
+  // Add the redirects function
+  async redirects() {
+    return [
+      {
+        source: '/dev/:slug*', // Matches any article under /dev/
+        destination: '/theindiedev/:slug*', // Redirects to the new path
+        permanent: true, // 301 redirect
+      },
+      {
+        source: '/design/:slug*', // Matches any article under /design/
+        destination: '/theindiedev/:slug*', // Redirects to the new path
+        permanent: true, // 301 redirect
+      },
+    ];
+  },
 }
  
 const withMDX = createMDX({
