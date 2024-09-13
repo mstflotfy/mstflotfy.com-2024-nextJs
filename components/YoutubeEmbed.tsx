55a2e55787a74adb152958a014cbeb2eae7e9cf8
mstflotfy <mstf.lotfy@gmail.com>
2024-09-13 16:16:03 +0300

    fix resizing issues

--- components/YoutubeEmbed.tsx
+++ components/YoutubeEmbed.tsx
@@ -7,10 +7,10 @@ const YoutubeEmbed: React.FC<YoutubeEmbedProps> = ({
 }) => {
   return (
     <div
-      className="pt-[6.4rem] pb-[8.4rem] px-[6.2rem] h-[62vw]"
+      className="sm:mx-[1.2rem] mt-[6.4rem] mb-[8.9rem] md:pt-[6.4rem] md:pb-[8.4rem] md:px-[6.2rem] h-[62vw] xl:h-[52vw] xl:px-[10vw] "
     >
       <iframe 
-        className="w-[100%] h-[100%] rounded-[4.6rem]"
+        className="w-[100%] h-[100%] rounded-[0.8rem] sm:rounded-[3.3] lg:rounded-[4.2rem]"
         src={`https://www.youtube-nocookie.com/embed/${videoId}`} 
         title="YouTube video player" 
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
