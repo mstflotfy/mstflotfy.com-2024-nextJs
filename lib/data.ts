a502b3bd9965f1de4613fb3b4e2507ef30c5fba2
mstflotfy <mstf.lotfy@gmail.com>
2024-09-27 21:46:10 +0300

    add correct source vids

--- lib/data.ts
+++ lib/data.ts
@@ -55,51 +55,51 @@ export const oneexerciseFeatures: Feature[] = [
 export const oneExerciseContent: Feature[] = [
   {
     name: 'Intro',
-    description: `Hey there, my name is Mostafa Lotfy, an indie app maker. I used to track my workouts manually or through different apps that provided basic history.
-    \nI like to start slowl and build up my progress, reviewing each workout to see how many reps or how much weight I used previously.
-    \nSo, I created a minimal workout tracker, OneExercise, that gives instant feedback for each set, while also providing quick access to the full history and some additional simple stats. This way, I can focus on improving one of these stats with each workout.
+    description: `Hey there, my name is Mostafa Lotfy (an indie app maker). I used to track my workouts manually or through apps that provided basic history.
+    \nI like to start slow and build up my progress, reviewing each workout to see how many reps or how much weight I used previously.
+    \nSo, I created a minimal workout tracker, OneExercise, that gives instant feedback for each set, while also providing quick access to the full history and some additional simple stats. This way, I can try to improve one of these stats with each workout.
     \nIn this quick guide, I will show you how to use OneExercise to easily track your workouts.`,
-    src: '/images/OneExercise-feat-free.jpg'
+    src: ''
   },
   {
     name: 'The Calander',
     description: `The calander marks all the days you have added at least one workout. \nYou can also navigate to any day you want by selecting it.`,
-    src: '/images/OneExercise-feat-free.jpg'
+    src: '/videos/OneExercise-calendar.mp4'
   },
   {
     name: 'Add a new workout',
     description: 'Go to the date you want to add a workout at (by default its set to today). Click on the `Add a new workout` button. \nSelect one exercise for your workout. OneExercise is a single exercise workout tracker (each workout is made up of one exercise).',
-    src: '/images/OneExercise-content-new-workout.jpg'
+    src: '/videos/add-new-OneExercise-workout.mp4'
   },
   {
     name: 'Check the history',
     description: 'You might want to check the history before you start your first set to know how many reps to aim for, for example. \nJust click on the history tab and you\'ll find a list of all the days you had a workout for the current selected exercise.',
-    src: '/images/OneExercise-content-new-workout.jpg'
+    src: '/videos/check-OneExercise-workout-history.mp4'
   },
   {
     name: 'Add a new set',
     description: `Just add your reps and weight for each set and press on Add set. It'll be added to your sets list for this workout. \nAnd you'll see if you've improved over the previous set of the last time you workedout this exercise.`,
-    src: '/images/OneExercise-content-new-workout.jpg'
+    src: '/videos/.mp4'
   },
   {
     name: 'Edit/Delete a set',
     description: `By pressing on any set in your current set list, you can change its current values then press update. Also a del button will appear to delte the selected set. You can press on the set again to deselect it after you finsih editing.`,
-    src: '/images/OneExercise-content-new-workout.jpg'
+    src: '/videos/edit-or-delete-OneExercise-workout-set.mp4'
   },
   {
-    name: 'Check stats',
+    name: 'Check quick workout stats',
     description: `Press on the stats button in you sets list to have a look at some extra simple stats, also color coded based on the last workout. You might want to improve on these if you're a nerd or if you fail to improve in other ways.`,
-    src: '/images/OneExercise-content-new-workout.jpg'
+    src: '/videos/view-OneExercise-workout-quick-stats.mp4'
   },
   {
     name: 'Update workout info',
     description: `You can press on the info tab to change the date of the current workout you're adding, or change the workout's exercise entirely.`,
-    src: '/images/OneExercise-content-new-workout.jpg'
+    src: '/videos/edit-OneExercise-workout-info.mp4'
   },
   {
     name: 'Workouts autosave',
     description: `Each time you add a set the workout is saved or updated. So if you're already added your sets your workout is already saved. And you can safely navigate back or close the app.`,
-    src: '/images/OneExercise-content-new-workout.jpg'
+    src: '/videos/OneExercise-workouts-autosaves.mp4'
   },
   {
     name: 'Add a new exercise',
@@ -108,7 +108,7 @@ export const oneExerciseContent: Feature[] = [
     \nThen click on the \`Add Exercise\` Button. Add a name for your exercise, the name must be unique, you can’t add an exercise with a name that already exists in your Exercise list. 
     
     \nIf you change your mind and decide not to add a new exercise, jut click on the back button and nothing will be saved.`,
-    src: '/addNewExercise_extraCompressed.mp4'
+    src: '/videos/add-new-exercise-in-OneExercise-app.mp4'
   },
   {
     name: 'Edit an exercise',
@@ -117,42 +117,42 @@ export const oneExerciseContent: Feature[] = [
     \nThen click on the \`Add Exercise\` Button. Add a name for your exercise, the name must be unique, you can’t add an exercise with a name that already exists in your Exercise list. 
     
     \nIf you change your mind and decide not to add a new exercise, jut click on the back button and nothing will be saved.`,
-    src: '/images/OneExercise-content-new-exercise.jpg'
+    src: '/videos/edit-OneExercise-exercises.mp4'
   },
   {
     name: 'The stats tab',
     description: `Head to the stats page by pressing on the stats tab in the bottom nav. You'll find a start and end dates for the period you want your stats for. By default the start date is set to the date of your very first workout, and the end date is today, however you can get stats for a custom period if you want. \nCurrently you get simple stats...`,
-    src: '/images/OneExercise-content-new-exercise.jpg'
+    src: '/videos/check-your-OneExercise-overall-stats.mp4'
   }
   ,
     {
       name: 'Switch color mode',
       description: `Open the side nav. You can quickly switch to dark/light mode via the switch. Or you can go to the setting and select the mode via drop down. If you select system OneExercise color mode will follow the settings of your browser.`,
-      src: '/images/OneExercise-content-new-exercise.jpg'
+      src: '/videos/switch-OneExercise-app-color-mode.mp4'
     }
     ,
       {
         name: 'Back up your workouts & exercises',
         description: `Open the side nav. Go to 'Settings' and press on Export this will save a file that looks like this on your device. You can later use this file to import (prehaps to a new browser) by pressing on the import button and selecting this file from your device. This will save/load all your workouts and exercises.`,
-        src: '/images/OneExercise-content-new-exercise.jpg'
+        src: '/videos/backup-OneExercise-workouts-&-exercises.mp4'
       }
       ,
         {
           name: 'Start over',
           description: `If for some reason, you wnat to delete all your progress and start with a blank slate. You could just head to setting and press on Reset. Careful this will delete all your workouts and exercises. Perhaps you should export your data first.`,
-          src: '/images/OneExercise-content-new-exercise.jpg'
+          src: '/videos/delete-all-OneExercise-workouts-&-exercises.mp4'
         }
         ,
           {
             name: 'Reset Presets',
             description: `If for some reason you've edited your preset exercises that come with OneExercise yet you would like to revert to the default options without deleting your other workouts and exercises. You can head to settings, then press on Reset presets.`,
-            src: '/images/OneExercise-content-new-exercise.jpg'
+            src: '/videos/restore-OneExercise-deleted-presets.mp4'
           }
           ,
             {
               name: 'Ask for help',
               description: `Have any questions about OneExercise?
               \n If you find anything confusing, annoying, or have any feedback, or feature requests, feel free to contact me by email.`,
-              src: '/images/OneExercise-content-new-exercise.jpg'
+              src: ''
             }
 ]
